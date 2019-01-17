66bd2d32a8615178d9baf263c14a27d0a3d2ca88
Patrick Näf <herzbube@herzbube.ch>
2019-01-17 04:16:06 +0100

    BulkyFiveNakade() no longer passes a temporary SgPointSet object to iterator (fixes #21)

--- go/GoEyeUtil.cpp
+++ go/GoEyeUtil.cpp
@@ -131,8 +131,9 @@ GoEyeStatus BulkyFiveNakade(const GoBoard& bd,
         // check if both empty points adjacent to block. if yes, nakade.
         // if no, alive.
         {
-            SG_ASSERT((points & bd.AllEmpty()).IsSize(2));
-            for (SgSetIterator it(points & bd.AllEmpty()); it; ++it)
+            SgPointSet empty = (points & bd.AllEmpty());
+            SG_ASSERT(empty.IsSize(2));
+            for (SgSetIterator it(empty); it; ++it)
             {
                 if (bd.NumNeighbors(*it, stoneColor) == 0)
                 {
